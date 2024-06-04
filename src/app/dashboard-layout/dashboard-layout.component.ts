bb6924e10addc363fa3a989401203ed503b6469c
Bruno Amezcua <amezcua_bruno@hotmail.com>
2024-06-03 18:48:41 -0600

    Added sidebar menu

--- src/app/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard-layout/dashboard-layout.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [ SidebarModule, ButtonModule ],
+  imports: [
+    SidebarModule,
+    ButtonModule,
+    AvatarModule
+  ],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.scss'
 })

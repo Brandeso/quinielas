03ccb81f4d829cd4b5bd39c91a3703f520798ab6
Bruno Amezcua <amezcua_bruno@hotmail.com>
2024-06-03 18:33:28 -0600

    Added Sidebar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { environment } from '../../environments/environment';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { provideAnimations } from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    { provide: FIREBASE_OPTIONS, useValue: environment.firebase }
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+    provideAnimations()
   ]
 };

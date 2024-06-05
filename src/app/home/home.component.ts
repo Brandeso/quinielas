c3ccb817e75014ea6620800432f7f5d59274c054
Bruno Amezcua <amezcua_bruno@hotmail.com>
2024-06-05 00:45:30 -0600

    Added cards and responsive ordering (#3)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { WeekService } from '../../services/weeks.service';
 import { Week } from '../../interfaces/week';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card'
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    CardModule,
+    ButtonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -21,8 +28,24 @@ export class HomeComponent implements OnInit {
     this.weekSrv.getAll().subscribe((res) => {
       this.weeks = [];
       res.forEach((doc) => {
-        console.log(doc.payload.doc.data());
+        const data: any = doc.payload.doc.data();
+        this.weeks.push({
+          id: doc.payload.doc.id,
+          no: data.no,
+          start: data.start,
+          end: data.end,
+          year: data.year,
+          matches: []
+        })
+        console.log(new Date(data.start))
       })
+      this.weeks.sort(this.sortWeeks)
     })
+  }
+
+  sortWeeks(a:Week, b:Week) {
+    if(a.no < b.no) return -1
+    else if (a.no > b.no) return 1
+    else return 0
   }
 }
